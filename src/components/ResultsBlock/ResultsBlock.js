f367ed9854db97f87d6778901f08d97f5a47c265
Zach Steiner <zsteiner@gmail.com>
2018-12-31 10:10:43 -0700

    Fix scaling function

--- src/components/ResultsBlock/ResultsBlock.js
+++ src/components/ResultsBlock/ResultsBlock.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import pluralize from 'pluralize';
-
 import units from '../../consts/units';
+import round from '../../utils/round';
 
 import styles from '../DilutionResults/DilutionResults.module.scss';
 
 const ResultsBlock = ({ amount, unit, liquid, showPlus }) => {
   const formattedUnit = unit ? pluralize(unit, amount) : null;
-
+  const formattedAmount = round(amount, 2);
   return (
     <React.Fragment>
       {showPlus ? <span> + </span> : null}
       <div>
-        <span className={styles.resultsNumber}>{amount} </span>
+        <span className={styles.resultsNumber}>{formattedAmount} </span>
         <span className={styles.unit}>{formattedUnit}</span> {liquid}
       </div>
     </React.Fragment>
--- src/utils/scale.js
+++ src/utils/scale.js
@@ -2,10 +2,15 @@ import normalizeUnits from './normalizeUnits';
 import setZero from './setZero';
 import units from '../consts/units';
 
+function scaleAmount(amount, scaleFactor) {
+  const scaledAmount = amount * setZero(scaleFactor);
+  return scaledAmount;
+}
+
 export default function scale(ingredients, scaleFactor) {
   const ingredientsFormatted = [];
 
-  const regex = /([0-9]*.?\/?[0-9]*)\s?(\S*)?\s?(.*)/;
+  const regex = /([0-9]*\.?\/?[0-9]*)\s?(\S*)?\s?(.*)/;
   const fractionRegex = /([0-9]*)\/([0-9]*)/;
 
   const cleanedIngredients = ingredients.filter(item => item !== '');
@@ -13,30 +18,35 @@ export default function scale(ingredients, scaleFactor) {
   cleanedIngredients.map(item => {
     let unit;
     let ingredient;
+    let amount;
 
     const normalizeItem = item.replace(' of ', ' ');
     const match = normalizeItem.match(regex);
     const hasThree = match[3] !== '';
 
-    let amount = match[1];
-    unit = hasThree ? normalizeUnits(match[2]) : null;
-    ingredient = hasThree ? match[3] : match[2];
+    console.log('match', match);
 
+    amount = match[1]; // amount from string
+    unit = hasThree ? normalizeUnits(match[2]) : null; // if two strings after amount, set unit
+    ingredient = hasThree ? match[3] : match[2]; // if two strings after amount, set ingredient
+
+    // check unit exists, if not set all strings after amount to ingredient
     if (units.indexOf(unit) < 0) {
-      console.log('no match', unit);
       unit = null;
       ingredient = hasThree ? `${match[2]} ${match[3]}` : match[2];
     }
 
+    // check fraction, check that unit is blank, convert unit to number
     if (fractionRegex.test(amount)) {
       const numerator = parseInt(amount.match(fractionRegex)[1], 10);
       const denominator = parseInt(amount.match(fractionRegex)[2], 10);
-      amount = numerator / denominator;
+      amount = scaleAmount(numerator / denominator, scaleFactor);
+    } else if (amount === '') {
+      amount = null;
     } else {
       amount = parseFloat(amount);
+      amount = scaleAmount(amount, scaleFactor);
     }
-    console.log('amount', amount);
-    amount = amount * setZero(scaleFactor);
 
     ingredientsFormatted.push({
       amount,
