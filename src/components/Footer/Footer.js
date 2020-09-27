44d4d81f27147ded34d6de85f0fac322e4efbfcc
Zach Steiner <zsteiner@gmail.com>
2020-09-27 12:34:13 -0600

    Add current year to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import styles from './Footer.module.scss';
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const year = new Date().getFullYear();
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <section className={styles.footerSection}>
-        &copy; 2018 <a href="http://zachsteiner.com">Zach Steiner</a> &amp;{' '}
+        &copy; {year} <a href="http://zachsteiner.com">Zach Steiner</a> &amp;{" "}
         <a href="http://jackieglimp.com">Jackie Glimp</a>
       </section>
     </footer>
